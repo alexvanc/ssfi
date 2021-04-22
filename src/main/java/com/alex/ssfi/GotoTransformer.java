09b69aae24a56bea8a59680b944980bd6369c68f
alexvanc <kliosvseyy@gmail.com>
2021-04-22 20:33:49 +0800

    upload daily work

--- src/main/java/com/alex/ssfi/GotoTransformer.java
+++ src/main/java/com/alex/ssfi/GotoTransformer.java
@@ -7,10 +7,19 @@ import org.objectweb.asm.tree.IntInsnNode;
 import soot.*;
 import soot.baf.DoubleWordType;
 import soot.baf.WordType;
+import soot.baf.internal.BafLocalBox;
+import soot.grimp.internal.ExprBox;
+import soot.grimp.internal.GRValueBox;
+import soot.grimp.internal.ObjExprBox;
 import soot.jimple.AddExpr;
 import soot.jimple.AssignStmt;
 import soot.jimple.BreakpointStmt;
+import soot.jimple.EqExpr;
+import soot.jimple.FieldRef;
 import soot.jimple.GotoStmt;
+import soot.jimple.IdentityStmt;
+import soot.jimple.IfStmt;
+import soot.jimple.IntConstant;
 import soot.jimple.InvokeExpr;
 import soot.jimple.InvokeStmt;
 import soot.jimple.Jimple;
@@ -21,6 +30,13 @@ import soot.jimple.StaticInvokeExpr;
 import soot.jimple.Stmt;
 import soot.jimple.StringConstant;
 import soot.jimple.SwitchStmt;
+import soot.jimple.internal.ConditionExprBox;
+import soot.jimple.internal.IdentityRefBox;
+import soot.jimple.internal.ImmediateBox;
+import soot.jimple.internal.InvokeExprBox;
+import soot.jimple.internal.JimpleLocalBox;
+import soot.jimple.internal.RValueBox;
+import soot.jimple.internal.VariableBox;
 import soot.jimple.toolkits.typing.fast.BottomType;
 import soot.util.*;
 
@@ -43,15 +59,11 @@ public class GotoTransformer extends BodyTransformer {
 //		    throw new RuntimeException("couldn't find main() in mainClass");
 //		boolean isMainMethod=b.getMethod().isMain();
 ////		SootMethod thisMethod=b.getMethod().get;
-//		SootField gotoCounter;
-//		if (addedCounter) {
-//			gotoCounter=Scene.v().getMainClass().getFieldByName("gotoCounter");
-//		}else {
-//			gotoCounter=new SootField("gotoCounter", LongType.v(), Modifier.STATIC);
-//			Scene.v().getMainClass().addField(gotoCounter);
-//			addedCounter=true;
-//		}
+
+
 ////	
+//		Chain<SootField> allFields=b.getMethod().getDeclaringClass().getFields();
+//		System.out.println(allFields.size());
 		
 		//Scene.v().loadNecessaryClasses();
 //		Scene.v().loadClassAndSupport("java.io.FileWriter");
@@ -61,27 +73,61 @@ public class GotoTransformer extends BodyTransformer {
 //		for(int i=0;i<constructor.size();i++) {
 //			System.out.println(constructor.get(i).getSubSignature());
 //		}
+//		System.out.println("In Method: "+b.getMethod().getName());
+//		List<SootMethod> methods=b.getMethod().getDeclaringClass().getMethods();
+//		for(int i=0;i<methods.size();i++) {
+//			Body body=methods.get(i).getActiveBody();
+//			int number=body.getUnits().size();
+//			
+//			System.out.println("Body name: "+body.getMethod().getName()+" units nubmer: "+number);
+//			if(body.getMethod().getName().equals("main")) {
+//				Iterator<Unit> units=body.getUnits().snapshotIterator();
+//				while(units.hasNext()) {
+//					Unit unit=units.next();
+//					System.out.println(unit.toString());
+//				}
+////				System.out.println(options.toString());
+//			}
+//		}
 		
 		String methodSignature=b.getMethod().getSignature();
-		if (!methodSignature.contains("calculate")){
+		if (!methodSignature.contains("tryAndCatch")){
 			return;
 		}
-		Chain<Unit> units=b.getUnits();
-		Iterator<Unit> stmIt=units.snapshotIterator();
-		while(stmIt.hasNext()) {
-			Stmt stmt=(Stmt)stmIt.next();
-			if (stmt instanceof SwitchStmt){
-				SwitchStmt switchStmt=(SwitchStmt)stmt;
-				List<Unit> targets=switchStmt.getTargets();
-				for (int i=0;i<targets.size();i++) {
-					System.out.println(targets.get(i).toString());
-				}
-				
-
-			}else if(stmt instanceof BreakpointStmt) {
-				System.out.println("break");
-			}
+		Iterator<Trap> trapItr=b.getTraps().snapshotIterator();
+		while(trapItr.hasNext()){
+			Trap tmp=trapItr.next();
+			System.out.println(tmp.getBeginUnit().toString());
+			System.out.println(tmp.getEndUnit().toString());
+			System.out.println(tmp.getHandlerUnit().toString());
 		}
+//		
+//		SootField gotoCounter;
+//		try {
+//			gotoCounter=Scene.v().getMainClass().getField("sootInjected",BooleanType.v());
+//		}catch(Exception e) {
+//			gotoCounter=new SootField("gotoCounter", LongType.v(), Modifier.STATIC);
+//			b.getMethod().getDeclaringClass().addField(gotoCounter);
+//		}
+//		
+//		Chain<Unit> units=b.getUnits();
+//		Iterator<Unit> stmIt=units.snapshotIterator();
+//		while(stmIt.hasNext()) {
+//			Stmt stmt=(Stmt)stmIt.next();
+//			if(stmt instanceof InvokeStmt) {
+//				EqExpr compareExp=Jimple.v().newEqExpr(Jimple.v().newStaticFieldRef(gotoCounter.makeRef()), IntConstant.v(1));
+//				IfStmt checkCondition=Jimple.v().newIfStmt(Jimple.v().newConditionExprBox(compareExp).getValue(), stmt);
+//				AssignStmt injectFlagStmt=Jimple.v().newAssignStmt(Jimple.v().newStaticFieldRef(gotoCounter.makeRef()), IntConstant.v(1));
+//				GotoStmt afterElseStmt=Jimple.v().newGotoStmt(units.getSuccOf(stmt));
+//				units.insertBefore(checkCondition, stmt);
+//				units.insertAfter(injectFlagStmt, checkCondition);
+//				units.insertAfter(afterElseStmt, injectFlagStmt);
+//			}
+//			
+//			
+//
+//		}
+		
 				
 		
 //		List<SootMethod> methods=b.getMethod().getDeclaringClass().getMethods();
